a1df8f21cbb0fa4c32962b48c48aa74844919948
seojoo <seojoo21@naver.com>
2022-08-01 23:56:13 +0900

    react.js 기초 연습 2 (useEffect, fetch()로 API 호출)

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,24 @@
-import dummy from '../db/data.json';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function DayList(){
-    console.log(dummy);
+
+    const [days, setDays] = useState([]); // 빈 배열로 만든다. 
+    
+    useEffect( () => {
+        fetch('http://localhost:3001/days') // fetch 로 API 경로 지정. promise를 반환하므로 then을 사용 
+        .then( res => {
+            console.log(res);
+            return res.json() // response는 http응답이므로 실제 json이 아니기 때문에 json()을 이용해 json으로 변환하고 Promise를 반환한다. 
+        })
+        .then ( data => {
+            console.log(data);
+            setDays(data);
+        })
+    }, []); // 상태값과 무과하게 렌더링 직후 한 번만 실행되는 작업은 빈 배열 []을 넣는다. 
+
     return <ul className="list_day">
-        {dummy.days.map(day => (
+        {days.map(day => (
             <li key={day.id}>
                 <Link to={`/day/${day.day}`}>Day {day.day}</Link>
             </li>
